687c95598b853be763be8c5c7ca62eb712c2d9f4
Luna <boreum4151@gmail.com>
2019-04-12 15:49:08 +0900

    add styled-components, remove css and use styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
+import React, { Component} from 'react';
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <button className="button button--success">Hello</button>
-        <button className="button button--danger">Hello</button>      
-      </Fragment>
+      <Container>
+        <Button danger>버튼이다@</Button>
+        <Button > HELLO</Button>     
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+height:100vh;
+width:100%;
+background-color:#ff7675;
+`;
+
+const Button = styled.button`
+border-radius: 50px;
+padding: 5px;
+min-width: 120px;
+color: white;
+font-weight: 600;
+-webkit-appearance: none;
+cursor: pointer;
+
+&:active,
+&:focus{
+  outline:none;
+}
+background-color: ${props=> (props.danger ? "#e74c3c":"#27ae60")};
+
+`;
 
 export default App;
