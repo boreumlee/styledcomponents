5faeead48b6a7a24d5097f335344ecf6dd3af656
Luna <boreum4151@gmail.com>
2019-04-12 16:59:57 +0900

    use theme provider

--- src/App.js
+++ src/App.js
@@ -1,59 +1,37 @@
 import React, { Component} from 'react';
-import styled, { css,keyframes } from 'styled-components';
+import styled,{css,ThemeProvider} from 'styled-components';
+import theme from './theme';
+
+const Container = styled.div`
+  height:100vh;
+  width:100%;
+  background-color:#ff7675;
+`;
+
+const Card = styled.div`
+  background-color: red;
+`
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor} 
+`
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Button danger>버튼이다@</Button>
-        <Button > HELLO</Button>
-        <Anchor href="http://google.com">go to 구글</Anchor>     
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
 
-const Container = styled.div`
-height:100vh;
-width:100%;
-background-color:#ff7675;
-`;
-
-const Button = styled.button`
-border-radius: 50px;
-padding: 5px;
-min-width: 120px;
-color: white;
-font-weight: 600;
--webkit-appearance: none;
-cursor: pointer;
-
-&:active,
-&:focus{
-  outline:none;
-}
-background-color: ${props=> (props.danger ? "#e74c3c":"#27ae60")};
-
-${props=> {
-  if(props.danger){
-    return css `animation:${rotation} 2s linear infinite`
-  }
-
-}}
-
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg)
-  } 
-  to {
-    transform: rotate(360deg)
-  }
-`
+const Form = () => (
+  <Card><Button> Hello </Button></Card>
+)
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+    mainColor: "#74b9ff",
+    dangerColor: "#d63031",
+    successColor: "#00b894"
+}
+
+export default theme;
